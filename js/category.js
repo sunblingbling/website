46210ce61d575ee2b9e38720633d7cd39cabbfa7
sunblingbling <sbb.in.jp@gmail.com>
2020-12-29 19:40:47 +0900

    필터탭 기능 추가

--- js/category.js
+++ js/category.js
@@ -1,24 +1,31 @@
-var $filters = $('.filter [data-filter]'),
-    $boxes = $('.mockup_list [data-category]');
+(function($) {
 
-$filters.on('click', function(e) {
-  e.preventDefault();
-  var $this = $(this);
-  $filters.removeClass('active');
-  $this.addClass('active');
-
-  var $filterColor = $this.attr('data-filter');
-
-  if ($filterColor == 'all') {
-    $boxes.removeClass('is-animated')
-      .fadeOut().promise().done(function() {
-        $boxes.addClass('is-animated').fadeIn();
-      });
-  } else {
-    $boxes.removeClass('is-animated')
-      .fadeOut().promise().done(function() {
-        $boxes.filter('[data-category = "' + $filterColor + '"]')
-          .addClass('is-animated').fadeIn();
-      });
-  }
-});
+    'use strict';
+  
+    var $filters = $('.tag [data-filter]'),
+      $boxes = $('.list_wrap [data-color]');
+  
+    $filters.on('click', function(e) {
+      e.preventDefault();
+      var $this = $(this);
+      
+      $filters.removeClass('active');
+      $this.addClass('active');
+  
+      var $filterColor = $this.attr('data-filter');
+  
+      if ($filterColor == 'all') {
+        $boxes.removeClass('is-animated')
+          .fadeOut().promise().done(function() {
+            $boxes.addClass('is-animated').fadeIn();
+          });
+      } else {
+        $boxes.removeClass('is-animated')
+          .fadeOut().promise().done(function() {
+            $boxes.filter('[data-color = "' + $filterColor + '"]')
+              .addClass('is-animated').fadeIn();
+          });
+      }
+    });
+  
+  })(jQuery);
